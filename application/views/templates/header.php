f12ba6ca96390bebb05077c35eba8ff5b0bf4648
ramafikri02 <ramafikri02@gmail.com>
2020-08-31 22:43:08 +0700

    ganti nama file css

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -30,7 +30,7 @@
     <link href="<?= base_url('assets/vendor/perfect-scrollbar/perfect-scrollbar.css') ?>" rel="stylesheet" media="all">
 
     <!-- Main CSS-->
-    <link href="<?= base_url('assets/css/st.css') ?>" rel="stylesheet" media="all">
+    <link href="<?= base_url('assets/css/style.css') ?>" rel="stylesheet" media="all">
 
     <!-- JS, Popper.js, and jQuery -->
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
